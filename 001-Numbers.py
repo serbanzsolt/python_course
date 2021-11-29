317e1ecad1a1a02e9bf4701d28c9b69040e0c055
Zsolt-Work <serban.zsolt@gmail.com>
2021-11-29 15:17:33 +0100

    Added numbers practice

---
+++ 001-Numbers.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 27 18:59:48 2021
+
+@author: Zsolt
+"""
+
+print(2 + 2)
+print(6 / 3)
+print(7 + 3)
+print(50 % 5)
+print(23 + 2)
+print(2 ** 2)
+print(2 + 10 * 10 + 2)
