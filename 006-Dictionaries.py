12a587c5c2137220740e790b5069f625fbabfade
Zsolt-Work <serban.zsolt@gmail.com>
2021-11-29 15:20:56 +0100

    Added Dictionaries practice

---
+++ 006-Dictionaries.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 28 14:20:11 2021
+
+@author: Zsolt
+"""
+
+# =============================================================================
+# Dictionaries {'key1':'value1', 'key2':'value2' } KEY - VALUE PAIRS
+# Unordered
+# =============================================================================
+
+my_dict = {'key1':'value1', 'key2':'value2' }
+print(my_dict)
