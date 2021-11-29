b9682803cd201033925792cc9211b997db20a838
Zsolt-Work <serban.zsolt@gmail.com>
2021-11-29 15:18:10 +0100

    Added variables.py

---
+++ 002-Variables.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 27 19:05:02 2021
+
+@author: Zsolt
+"""
+
+a = 10
+print(a)
+
+a = a+a
+print(a)
+
+print(type(a))
+
+a = 30.1
+print(type(a))
+
+my_income = 100
+
+tax_rate = 0.1
+
+my_taxes = my_income*tax_rate
+
+print(my_taxes)
+
