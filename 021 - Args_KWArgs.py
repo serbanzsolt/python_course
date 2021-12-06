146794390d70bab20bc9fb27a1071b0bca570d37
Zsolt-Home <serban.zsolt@gmail.com>
2021-12-06 22:31:29 +0100

    finishing args and kwargs

--- 021 - Args_KWArgs.py
+++ 021 - Args_KWArgs.py
@@ -9,6 +9,7 @@ Created on Mon Dec  6 02:28:57 2021
 # *args and **kwargs - Arguments and KeyWord Arguments
 # =============================================================================
 
+# *args
 def myfunc (a,b):
     # returns 5% of the sum of a and b
     return sum((a,b)) * 0.05
@@ -21,3 +22,23 @@ def myfunc2 (*args):
 
 print(myfunc2(10,2123,1,212,12,34))
 print(myfunc2(20,3,50))
+
+# **kwargs is just a dictionary
+
+def myfunc3(**kwargs):
+    print(kwargs)
+    if 'fruit' in kwargs:
+        print('MY fruit of choice is {}'.format(kwargs['fruit']))
+    else:
+        print('I did not find any fruit here')
+        
+myfunc3(fruit='apple', veggie='lettuce')
+
+# combine these
+
+def myfunc4 (*args, **kwargs):
+    print(args)
+    print(kwargs)
+    print('I would like {} {}'.format(args[0],kwargs['food']))
+    
+myfunc4(10,20,30,fruit='orange',food='eggs',animal='dog')
